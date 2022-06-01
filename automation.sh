7fbf78c899f4094e774ecae5bdf21514596ea26c
MaharshiNaveen <pmnaveen02@gmail.com>
2022-06-01 15:05:45 +0000

    adding auto bookloging and cronjob features version 0.2

--- automation.sh
+++ automation.sh
@@ -1,9 +1,33 @@
-#version 0.1
+#version 0.2
 #.....initializing time, service name and bucket name.....#
 timestamp=$(date '+%d%m%Y-%H%M%S')
 service=apache2
 myname=pasupuleti
 s3_bucket=upgrad-pasupuleti
+
+#.....initializing time service name and bucket name.....#
+if [ -e /etc/cron.d/automation ]
+then
+crontab /etc/cron.d/automation
+echo "automation file exists"
+else
+touch /etc/cron.d/automation
+echo "SHELL=/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+#cron job to perform automation script
+* * * * *  root /root/Automation_Project/automation.sh
+" >> /etc/cron.d/automation
+crontab /etc/cron.d/automation
+fi
+#.....Bookkeeping inventory creation.....#
+if [ -e /var/www/html/inventory.html ]
+        then
+        echo "Inventory exists"
+        else
+        touch /var/www/html/inventory.html
+        cd /var/www/html/
+        echo "<b>Log Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Date Created &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp; Size &nbsp</b><br>" >> inventory.html
+fi
 
 function automate {
 #.....directing to log archive location....#
@@ -31,6 +55,11 @@ aws s3 \
 cp  ${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}
 
+#.....bookkeeping the log archive.....#
+cd /var/www/html/
+filesize=$(ls -lh /tmp/apache2_log_bkp/${myname}-httpd-logs-${timestamp}.tar | awk '{print  $5}')
+echo "<p >httpd-logs &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp; ${filesize} &nbsp;&nbsp </p><br>" >> inventory.html
+
 }
 
 
@@ -44,6 +73,7 @@ echo "installing $service"
 fi
 
 #.....condition which checks the service is running or not
+#if (( ! $(ps -ef | grep $service | wc -l) > 1 ))
 if (( ! $(ps -eo comm,etime,user | grep root | grep $service | wc -l ) > 0 ))
 then
 #......starting the apache server if not started......#
@@ -51,12 +81,11 @@ then
 echo "$service is started!"
 fi
 
-#.......check the apache service is running and go with the tasks........#
 if [ -e "/etc/init.d/$service" ] && (( $(ps -eo comm,etime,user | grep root | grep $service | wc -l ) > 0 ))
 then
 automate
 else
-echo "error" 
+echo "not automating"
 fi
 
 
